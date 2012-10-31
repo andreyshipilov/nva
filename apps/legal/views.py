cef0b6a1498127c99ab6e42d4e580a52434bb4ce
Andrey Shipilov <a@andreyshipilov.com>
2012-11-01 05:36:48 +1030

    Typo.

--- apps/legal/views.py
+++ apps/legal/views.py
@@ -4,7 +4,7 @@ from .models import Patent, License
 
 
 class Index(TemplateView):
-    template_name = "lecnses_patents.html"
+    template_name = "lecenses_patents.html"
     
     def get_context_data(self, **kwargs):
         context = super(Index, self).get_context_data(**kwargs)
