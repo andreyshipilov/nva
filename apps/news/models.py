db405d1c8ffc634c6b40df5a2919c6a194640d85
Andrey Shipilov <a@andreyshipilov.com>
2012-11-01 16:57:51 +1030

    Fixes. Closes #19..

--- apps/news/models.py
+++ apps/news/models.py
@@ -5,19 +5,23 @@ from mezzanine.core.fields import RichTextField
 from mezzanine.core.models import Displayable
 from mezzanine.core.managers import PublishedManager
 from projects.models import Project
+from work.models import Product
 
 
 class NewsItem(Displayable):
     full_text = RichTextField(verbose_name=u'текст новости')
-    project = models.ForeignKey(Project, blank=True,
-                                help_text=u'Проект, с которым связана новость.',)
+    product = models.ForeignKey(Product, blank=True, null=True,
+                                verbose_name=u'продукт, с которым связана новость',)
+    project = models.ForeignKey(Project, blank=True, null=True,
+                                verbose_name=u'проект, с которым связана новость',)
+
     objects = PublishedManager()
-    
+
     class Meta:
         ordering = ('-publish_date',)
         verbose_name = u'новость'
         verbose_name_plural = u'новости'
-    
+
     def __unicode__(self):
         return self.title
 
